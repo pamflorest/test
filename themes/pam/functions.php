fc74f30c2f227b188f2cf38c3333949e249dee1d
edgar <edgar@losmaquiladores.com>
2014-03-20 10:31:29 -0600

    taxonomias y post type creados desde office

--- themes/pam/functions.php
+++ themes/pam/functions.php
@@ -1,57 +1,111 @@
 <?php
+	/**
+	 * Proper way to enqueue scripts and styles
+	 */
+	function theme_name_scripts() {
+		wp_enqueue_style( 'style', get_stylesheet_uri() );
+		//wp_enqueue_script( 'script-name', get_template_directory_uri() . '/js/example.js', array(), '1.0.0', true );
+	}
+
+	add_action( 'wp_enqueue_scripts', 'theme_name_scripts' );
+
+	/****PARA LAS IMAGENES ****/
+	add_theme_support( 'post-thumbnails' );
+
+	/***** PARA LAS TAXONOMIAS ****/
+	function animal_init() {
+	// create a new taxonomy
+		register_taxonomy(
+			'animal',
+			'post',
+			array(
+				'hierarchical'      => true,
+				'label' => __( 'Animal' ),
+				'rewrite' => array( 'slug' => 'animal' ),
+				'capabilities' => array(
+					'manage__terms' => 'edit_posts',
+				    'edit_terms' => 'manage_categories',
+				    'delete_terms' => 'manage_categories',
+				    'assign_terms' => 'edit_posts'
+				)
+			)
+		);
+	}
+	add_action( 'init', 'animal_init' );
+
+	function planta_init() {
+	// create a new taxonomy
+		register_taxonomy(
+			'planta',
+			'post',
+			array(
+				'hierarchical'      => true,
+				'label' => __( 'Planta' ),
+				'rewrite' => array( 'slug' => 'planta' ),
+				'capabilities' => array(
+					'manage__terms' => 'edit_posts',
+				    'edit_terms' => 'manage_categories',
+				    'delete_terms' => 'manage_categories',
+				    'assign_terms' => 'edit_posts'
+				)
+			)
+		);
+	}
+	add_action( 'init', 'planta_init' );
+
+	function intereses_init() {
+	// create a new taxonomy
+		register_taxonomy(
+			'intereses',
+			'pam',
+			array(
+				'hierarchical'      => true,
+				'label' => __( 'Intereses' ),
+				'rewrite' => array( 'slug' => 'intereses' ),
+				'capabilities' => array(
+					'manage__terms' => 'edit_posts',
+				    'edit_terms' => 'manage_categories',
+				    'delete_terms' => 'manage_categories',
+				    'assign_terms' => 'edit_posts'
+				)
+			)
+		);
+	}
+	add_action( 'init', 'intereses_init' );
+
+	function compartida_init() {
+	// create a new taxonomy
+		register_taxonomy(
+			'compartida',
+			array('post','pam')
+			array(
+				'hierarchical'      => true,
+				'label' => __( 'Compartida' ),
+				'rewrite' => array( 'slug' => 'compartida' ),
+				'capabilities' => array(
+					'manage__terms' => 'edit_posts',
+				    'edit_terms' => 'manage_categories',
+				    'delete_terms' => 'manage_categories',
+				    'assign_terms' => 'edit_posts'
+				)
+			)
+		);
+	}
+	add_action( 'init', 'compartida_init' );
 
 
-
-// DEFINIR LOS PATHS A LOS DIRECTORIOS DE JAVASCRIPT Y CSS ///////////////////////////
-
-
-
-	define( 'JSPATH', get_template_directory_uri() . '/js/' );
-
-	define( 'CSSPATH', get_template_directory_uri() . '/css/' );
-
-	define( 'THEMEPATH', get_template_directory_uri() . '/' );
-
-	define( 'SITEURL', site_url('/') );
-
-
-
-// FRONT END SCRIPTS AND STYLES //////////////////////////////////////////////////////
-
-
-
-	add_action( 'wp_enqueue_scripts', function(){
-
-		// scripts
-		wp_enqueue_script( 'plugins', JSPATH.'plugins.js', array('jquery'), '1.0', true );
-		wp_enqueue_script( 'functions', JSPATH.'functions.js', array('plugins'), '1.0', true );
-
-		// localize scripts
-		wp_localize_script( 'functions', 'ajax_url', admin_url('admin-ajax.php') );
-
-		// styles
-		wp_enqueue_style( 'styles', get_stylesheet_uri() );
-
-	});
-
-
-
-// ADMIN SCRIPTS AND STYLES //////////////////////////////////////////////////////////
-
-
-
-	add_action( 'admin_enqueue_scripts', function(){
-
-		// scripts
-		wp_enqueue_script( 'admin-js', JSPATH.'admin.js', array('jquery'), '1.0', true );
-
-		// localize scripts
-		wp_localize_script( 'admin-js', 'ajax_url', admin_url('admin-ajax.php') );
-
-		// styles
-		wp_enqueue_style( 'admin-css', CSSPATH.'admin.css' );
-
-	});
-
-	add_theme_support( 'post-thumbnails' ); 
+	/***** POST TYPES *****/
+	add_action( 'init', 'create_post_type' );
+	function create_post_type() {
+		register_post_type( 'pam',
+			array(
+				'labels' => array(
+					'name' => __( 'Pam' ),
+					'singular_name' => __( 'Pam' )
+				),
+			'public' => true,
+			'has_archive' => true,
+			)
+		);
+	}
 ?>
